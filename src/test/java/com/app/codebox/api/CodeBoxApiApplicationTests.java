60b6a348d4409d25447f69e025b342e5fcca5e36
rinjyu <rinjyu@naver.com>
2020-12-18 18:59:16 +0900

    [feat]: codebox.api 생성
    codebox.api 생성

---
+++ src/test/java/com/app/codebox/api/CodeBoxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.codebox.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBoxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
